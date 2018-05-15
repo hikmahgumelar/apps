bbe61f8472cee3aedde568fb95ab4e3ce7766d15
hikmahgumelar <hikmahgumelar@gmail.com>
2018-05-16 00:19:28 +0700

    add fungsi todo with awngular

--- js/app.js
+++ js/app.js
@@ -5,4 +5,19 @@ app.controller('MainController',['$scope', ($scope) => {
     $scope.ikat = ['1,2,3,4,5,6,7,8,9,10'];
     $scope.tanggal = new Date();
     $scope.banyaknya =  ["1 Kg","2 Kg","3 Kg","4 Kg"] ;
+
+
+  $scope.datas = [
+  {text:'satu tuilsa', selesai:false},
+  {text:'dua tuilsa', selesai:false},
+  ];
+$scope.totalDatas = () => {
+  return $scope.datas.length;
+  }
+$scope.tambahDatas = () => {
+ $scope.datas.push({text:$scope.formTambahdata.text, selesai:false});
+ $scope.formTambahdata='';
+ }
+//akhir
+
 }])
