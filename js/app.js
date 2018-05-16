b20136e92cde17d4bfb10bf72433780fecd17c9e
hikmahgumelar <hikmahgumelar@gmail.com>
2018-05-16 11:34:06 +0700

    fungsi logic running versi 1

--- js/app.js
+++ js/app.js
@@ -5,18 +5,19 @@ app.controller('MainController',['$scope', ($scope) => {
     $scope.ikat = ['1,2,3,4,5,6,7,8,9,10'];
     $scope.tanggal = new Date();
     $scope.banyaknya =  ["1 Kg","2 Kg","3 Kg","4 Kg"] ;
-
-
-  $scope.datas = [
-  {text:'satu tuilsa', selesai:false},
-  {text:'dua tuilsa', selesai:false},
-  ];
+    $scope.hargasatuan = ["1000","2000"];
+    
+  $scope.datas = [];
 $scope.totalDatas = () => {
   return $scope.datas.length;
   }
 $scope.tambahDatas = () => {
- $scope.datas.push({text:$scope.desValue, selesai:false});
+ $scope.datas.push({Destext:$scope.desValue, Ikattext:$scope.ikatValue, Banyaknyatext:$scope.banyaknyaValue, Hargasatuantext:$scope.harsatuValue});
  $scope.desValue ='';
+ $scope.ikatValue='';
+ $scope.banyaknyaValue='';
+ $scope.harsatuValue='';
+$scope.TotalPendapatan = '';
  }
 //akhir
 
